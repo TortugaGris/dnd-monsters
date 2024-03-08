f79f6c894896ee6456699745ff1101c6fed44e3f
Javier Zavala <javierzavalatorres@gmail.com>
2024-03-08 11:43:52 -0300

    feat: add monster service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,19 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Signal, computed } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { MonsterService } from './data-access/monster.service';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     RouterModule,
+    JsonPipe,
   ],
   template: `
-    <p>
-      home works!
-    </p>
+    @if(monsterItem()) {
+      <pre>
+        {{monsterItem() | json}}
+      </pre>
+    }
   `,
   styles: [],
 })
 export class HomeComponent {
+  monsters = this.monsterService.monsters;
+  monsterList = this.monsterService.monsterList;
+  isLoaded = this.monsterService.isLoaded;
+
+  url: string | null = null;
+  randomUrl = computed(() => {
+    if(this.url !== null) return this.url
+    const monsterList = this.monsterList();
+    if(monsterList === null) return;
+    const monster = monsterList.results[Math.floor(Math.random() * monsterList.results.length)];
+    this.monsterService.loadItem$.next(monster.url);
+    this.url = monster.url
+    return monster.url;
+  })
+
+  monsterItem = computed(() => {
+    const url = this.randomUrl();
+    if(url === undefined) return undefined;
+    const monsters = this.monsterService.monsters();
+    return monsters.find((monster) => monster.url === url);
+  });
+
+  constructor(
+    private readonly monsterService: MonsterService,
+  ) {
+  }
 
 }
